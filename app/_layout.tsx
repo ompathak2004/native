c594a3905c78932f7470d506f566235a0f4e0df9
ompathak2004 <om2004.pathak@gmail.com>
2024-11-01 02:53:57 +0530

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { DownloadPicture } from '@/components/BottomSheet';
+import { useWallpapers } from '@/hooks/useWallpapers';
+import { Slot, Stack } from 'expo-router';
+import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
+export default function Layout() {
+    const walletpapers = useWallpapers();
+    return <GestureHandlerRootView>
+        <Stack screenOptions={{
+            headerShown: false
+        }} >
+        </Stack>
+    </GestureHandlerRootView>
+}
